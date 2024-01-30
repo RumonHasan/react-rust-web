056492e74a55a28cd9109a0ab4168485e4ee20b3
RumonHasan <rumonuniv@gmail.com>
2024-01-31 02:37:21 +0900

    creating a new user and pushing it to a app state

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,51 +1,73 @@
 use std::vec;
 use actix_cors::Cors;
-use actix_web::{ get, http, web::{self, Data}, App, HttpResponse, HttpServer, Responder };
+use actix_web::{
+    get,
+    http::{ self, StatusCode },
+    post,
+    web::{ self },
+    App,
+    HttpResponse,
+    HttpServer,
+    Responder,
+};
 use serde::{ Serialize, Deserialize };
-use std::sync::{Mutex, Arc};
+use std::sync::{ Mutex, Arc };
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
+// main struct
 struct User {
     username: String,
     email: String,
     age: u32,
 }
-
-struct AppState{
+// mutable shared app state that is responsible for passing the data
+struct AppState {
     // this is important for making the todos as immutable reference arcs
     pub users: Arc<Mutex<Vec<User>>>,
 }
-
+// implements of app state
 impl AppState {
-    // Method to initialize the state with random users
-    pub fn init_with_random_users()-> AppState{
-        let random_users = fetch_random_users();
+    pub fn return_curr_app_state()-> AppState{
         AppState{
+            users: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+    // Method to initialize the state with random users
+    pub fn init_with_random_users() -> AppState {
+        let random_users = fetch_random_users();
+        AppState {
             users: Arc::new(Mutex::new(random_users)),
         }
+    }
+    pub fn set_new_user(&self, new_user: User) {
+        // locking the users and checking whether they exist or not then only pushing it to the existing users 
+        let mut existing_users = self.users.lock().unwrap();
+        existing_users.push(new_user);
     }
 }
 // basic cors route
 #[get("/hi")]
 async fn index(data: web::Data<AppState>) -> impl Responder {
-    let mut  dereferenced_users = data.users.lock().unwrap();  // This is a clone of the Mutex, not the Vec<User>
-    let mut local_users: Vec<User> = Vec::new();
-    for item in dereferenced_users.iter_mut(){ // here item is a mutable reference of a user
-        item.age = 45;
-        local_users.push(item.clone());
-    }
-    // passing the dereferenced mutex users from App State as Vec
+    let dereferenced_users = data.users.lock().unwrap(); // This is a clone of the Mutex, not the Vec<User>
+    let local_users: Vec<User> = dereferenced_users.to_vec();
     HttpResponse::Ok().json(local_users)
 }
 
-fn fetch_random_users()-> Vec<User>{
-    let counter:i32 = 10;
+#[post("/post")]
+async fn post_users(body: web::Json<User>, data: web::Data<AppState>) -> impl Responder {
+    let new_user: User = body.into_inner(); // into inner is used to consumer the JSON body
+    data.get_ref().set_new_user(new_user.clone()); // passes the new set of users
+    HttpResponse::build(StatusCode::OK).body("You managed to post it... jeezus boi!")
+}
+
+fn fetch_random_users() -> Vec<User> {
+    let counter: i32 = 10;
     let mut users: Vec<User> = Vec::new();
-    for _ in 0..counter{
-        let single_user: User = User{
+    for _ in 0..counter {
+        let single_user: User = User {
             username: String::from("rumon"),
             email: "rumon@gmail.com".to_string(),
-            age: 27
+            age: 27,
         };
         users.push(single_user);
     }
@@ -55,7 +77,7 @@ fn fetch_random_users()-> Vec<User>{
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // creating an instance of appState
-    let app_state = AppState::init_with_random_users(); 
+    let app_state = AppState::return_curr_app_state(); // initial renders
     let app_data = web::Data::new(app_state);
 
     let server_result = HttpServer::new(move || {
@@ -69,9 +91,7 @@ async fn main() -> std::io::Result<()> {
             .allowed_header(http::header::CONTENT_TYPE)
             .max_age(3600);
 
-        App::new().app_data(app_data.clone())
-        .wrap(cors)
-        .service(index)
+        App::new().app_data(app_data.clone()).wrap(cors).service(index).service(post_users)
     })
         .bind(("127.0.0.1", 8080))?
         .run().await;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,56 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+export const LOCAL_PORT = 'http://127.0.0.1:8080';
+
 function App() {
   const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({
+    username: '',
+    email: '',
+    age: '',
+  });
 
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const { data } = await axios.get('http://127.0.0.1:8080/hi');
+        const { data } = await axios.get(`${LOCAL_PORT}/hi`);
         setUsers(data);
-        console.log(data);
       } catch (error) {
         console.log(error);
       }
     };
-
     getUsers();
   }, []);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNewUser({
+      ...newUser,
+      [name]: value,
+    });
+  };
+
+  // creating a new user
+  const createNewUser = async (e) => {
+    e.preventDefault();
+    try {
+      const check = [...Object.values(newUser)].every((val) => val !== '');
+      if (check) {
+        const newUserObject = {
+          username: newUser.username,
+          email: newUser.email,
+          age: Number(newUser.age),
+        };
+        const response = await axios.post(`${LOCAL_PORT}/post`, newUserObject);
+        if (response.data) {
+          console.log('successful');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -32,6 +66,27 @@ function App() {
             </div>
           );
         })}
+      </div>
+      <div>
+        <input
+          placeholder="username"
+          value={newUser.username}
+          name="username"
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="email"
+          value={newUser.email}
+          name="email"
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="age"
+          value={newUser.age}
+          name="age"
+          onChange={handleInputChange}
+        />
+        <button onClick={(e) => createNewUser(e)}>Add New User</button>
       </div>
     </>
   );
